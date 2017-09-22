0b9938103e8309fe3568bc1b2f47e8038c46b016
Alex <a_kiddo13@hotmail.com>
2017-09-22 08:10:47 +0600

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,312 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.namebox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.radioMale = new System.Windows.Forms.RadioButton();
+            this.radioFemale = new System.Windows.Forms.RadioButton();
+            this.radioNone = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.calendar = new System.Windows.Forms.DateTimePicker();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.nameColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mailColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genderColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.cityColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label9 = new System.Windows.Forms.Label();
+            this.mailbox = new System.Windows.Forms.TextBox();
+            this.displayGender = new System.Windows.Forms.TextBox();
+            this.displayDate = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Silver;
+            this.button1.Location = new System.Drawing.Point(256, 136);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Add";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // namebox
+            // 
+            this.namebox.Location = new System.Drawing.Point(82, 28);
+            this.namebox.Name = "namebox";
+            this.namebox.Size = new System.Drawing.Size(152, 20);
+            this.namebox.TabIndex = 1;
+            this.namebox.TextChanged += new System.EventHandler(this.display_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 143);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "City";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 114);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Gender";
+            // 
+            // radioMale
+            // 
+            this.radioMale.AutoSize = true;
+            this.radioMale.Location = new System.Drawing.Point(82, 114);
+            this.radioMale.Name = "radioMale";
+            this.radioMale.Size = new System.Drawing.Size(48, 17);
+            this.radioMale.TabIndex = 15;
+            this.radioMale.TabStop = true;
+            this.radioMale.Text = "Male";
+            this.radioMale.UseVisualStyleBackColor = true;
+            this.radioMale.CheckedChanged += new System.EventHandler(this.radioMale_CheckedChanged);
+            // 
+            // radioFemale
+            // 
+            this.radioFemale.AutoSize = true;
+            this.radioFemale.Location = new System.Drawing.Point(140, 114);
+            this.radioFemale.Name = "radioFemale";
+            this.radioFemale.Size = new System.Drawing.Size(59, 17);
+            this.radioFemale.TabIndex = 16;
+            this.radioFemale.TabStop = true;
+            this.radioFemale.Text = "Female";
+            this.radioFemale.UseVisualStyleBackColor = true;
+            // 
+            // radioNone
+            // 
+            this.radioNone.AutoSize = true;
+            this.radioNone.Location = new System.Drawing.Point(205, 114);
+            this.radioNone.Name = "radioNone";
+            this.radioNone.Size = new System.Drawing.Size(51, 17);
+            this.radioNone.TabIndex = 17;
+            this.radioNone.TabStop = true;
+            this.radioNone.Text = "None";
+            this.radioNone.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(84, 143);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(150, 30);
+            this.listBox1.TabIndex = 18;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(66, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Date of Birth";
+            // 
+            // calendar
+            // 
+            this.calendar.Location = new System.Drawing.Point(84, 88);
+            this.calendar.Name = "calendar";
+            this.calendar.Size = new System.Drawing.Size(150, 20);
+            this.calendar.TabIndex = 20;
+            this.calendar.ValueChanged += new System.EventHandler(this.calendar_ValueChanged);
+            // 
+            // table
+            // 
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameColumn,
+            this.mailColumn,
+            this.dateColumn,
+            this.genderColumn,
+            this.cityColumn});
+            this.table.Location = new System.Drawing.Point(28, 189);
+            this.table.Name = "table";
+            this.table.Size = new System.Drawing.Size(546, 150);
+            this.table.TabIndex = 23;
+            this.table.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.table_CellContentClick);
+            this.table.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.table_CellMouseUp);
+            // 
+            // nameColumn
+            // 
+            this.nameColumn.HeaderText = "Name";
+            this.nameColumn.Name = "nameColumn";
+            // 
+            // mailColumn
+            // 
+            this.mailColumn.HeaderText = "E-Mail";
+            this.mailColumn.Name = "mailColumn";
+            // 
+            // dateColumn
+            // 
+            this.dateColumn.HeaderText = "Date of Birth";
+            this.dateColumn.Name = "dateColumn";
+            // 
+            // genderColumn
+            // 
+            this.genderColumn.HeaderText = "Gender";
+            this.genderColumn.Name = "genderColumn";
+            // 
+            // cityColumn
+            // 
+            this.cityColumn.HeaderText = "City";
+            this.cityColumn.Name = "cityColumn";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(13, 61);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(36, 13);
+            this.label9.TabIndex = 24;
+            this.label9.Text = "E-Mail";
+            // 
+            // mailbox
+            // 
+            this.mailbox.Location = new System.Drawing.Point(82, 58);
+            this.mailbox.Name = "mailbox";
+            this.mailbox.Size = new System.Drawing.Size(152, 20);
+            this.mailbox.TabIndex = 25;
+            this.mailbox.TextChanged += new System.EventHandler(this.mailbox_TextChanged);
+            // 
+            // displayGender
+            // 
+            this.displayGender.Location = new System.Drawing.Point(256, 111);
+            this.displayGender.Name = "displayGender";
+            this.displayGender.Size = new System.Drawing.Size(100, 20);
+            this.displayGender.TabIndex = 12;
+            // 
+            // displayDate
+            // 
+            this.displayDate.Location = new System.Drawing.Point(256, 88);
+            this.displayDate.Name = "displayDate";
+            this.displayDate.Size = new System.Drawing.Size(100, 20);
+            this.displayDate.TabIndex = 22;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 26);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            this.contextMenuStrip1.Click += new System.EventHandler(this.contextMenuStrip1_Click);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(586, 360);
+            this.Controls.Add(this.mailbox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.displayDate);
+            this.Controls.Add(this.calendar);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioNone);
+            this.Controls.Add(this.radioFemale);
+            this.Controls.Add(this.radioMale);
+            this.Controls.Add(this.displayGender);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.namebox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox namebox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton radioMale;
+        private System.Windows.Forms.RadioButton radioFemale;
+        private System.Windows.Forms.RadioButton radioNone;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.DateTimePicker calendar;
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox mailbox;
+        private System.Windows.Forms.TextBox displayGender;
+        private System.Windows.Forms.TextBox displayDate;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mailColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genderColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn cityColumn;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem deleteToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public int RowIndex { get; private set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            string[] cities = { "Almaty", "Astana"};
+            listBox1.Items.AddRange(cities);
+            listBox1.SetSelected(0, true);
+            displayDate.Visible = false;
+            displayGender.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            
+
+            //displayCity.Text = listBox1.SelectedItem.ToString();
+
+            if (radioMale.Checked)
+            {
+                displayGender.Text = radioMale.Text;
+            }
+
+            if (radioFemale.Checked)
+            {
+                displayGender.Text = radioFemale.Text;
+            }
+
+            if (radioNone.Checked)
+            {
+                displayGender.Text = radioNone.Text;
+            }
+
+            displayDate.Text = calendar.Value.ToString("dd/MM/yyyy");
+
+            if (!string.IsNullOrWhiteSpace(mailbox.Text))
+            {
+                try
+                {
+                    var test = new MailAddress(mailbox.Text);
+                    table.Rows.Add(namebox.Text, mailbox.Text, displayDate.Text, displayGender.Text, listBox1.Text);
+                }
+                catch (FormatException ex)
+                {
+                    string message = "Не Email!!";
+                    string caption = "Ошибка";
+                    MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                    DialogResult result;
+
+                    result = MessageBox.Show(message, caption, buttons);
+                    
+                }
+            }
+
+            
+
+
+        }
+
+        private void display_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioMale_CheckedChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void calendar_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void mailbox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void deleteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void table_CellMouseUp(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                this.table.Rows[e.RowIndex].Selected = true;
+                this.RowIndex = e.RowIndex;
+                this.table.CurrentCell = this.table.Rows[e.RowIndex].Cells[1];
+                this.contextMenuStrip1.Show(this.table, e.Location);
+                contextMenuStrip1.Show(Cursor.Position);
+            }
+        }
+
+        private void contextMenuStrip1_Click(object sender, EventArgs e)
+        {
+            if (!this.table.Rows[this.RowIndex].IsNewRow)
+            {
+                this.table.Rows.RemoveAt(this.RowIndex);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
